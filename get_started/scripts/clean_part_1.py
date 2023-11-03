ddffba517cc279d46fa9848dc20dfaca4b0cbc2e
Stanley-Kubick <stanley.kubick@craft.ai>
2023-11-03 15:39:31 +0000

    chore(github): sync repositories

---
+++ get_started/scripts/clean_part_1.py
@@ -0,0 +1,22 @@
+import os
+from craft_ai_sdk import CraftAiSdk
+from craft_ai_sdk.exceptions import SdkException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+sdk = CraftAiSdk(
+    environment_url=os.environ.get("CRAFT_AI_ENVIRONMENT_URL"),
+    sdk_token=os.environ.get("CRAFT_AI_ACCESS_TOKEN"),
+)
+try:
+    sdk.delete_pipeline(
+        pipeline_name="part-1-hello-world", force_deployments_deletion=True,
+    )
+except SdkException as e:
+    print(e)
+try:
+    sdk.delete_step(step_name="part-1-hello-world")
+except SdkException as e:
+    print(e)
